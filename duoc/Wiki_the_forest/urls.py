187ef9f7954d07f89dfd23c877be6b4e901de22a
Amanda <aman.concha@duocuc.cl>
2025-04-04 18:48:09 -0300

    url foro

--- duoc/Wiki_the_forest/urls.py
+++ duoc/Wiki_the_forest/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import menu
+from .views import foro
 
 
 urlpatterns = [
     path('menu', menu, name="menu"),
+    path('menu/foro', foro, name="foro"),
 ]
--- duoc/Wiki_the_forest/views.py
+++ duoc/Wiki_the_forest/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def menu(request):
     return render(request, 'menuprincipal_wiki.html', {})
+
+def foro(request):
+    return render(request, 'forowiki.html', {})
+
