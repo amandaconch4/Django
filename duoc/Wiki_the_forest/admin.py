f6d6349b2870f0aab1f7787d3b4b107820be0672
Amanda <aman.concha@duocuc.cl>
2025-04-12 17:28:11 -0400

    admin listo y cambio del nombre de usuario en admin

--- duoc/Wiki_the_forest/admin.py
+++ duoc/Wiki_the_forest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Perfil
+from .models import usuario
 
 # Register your models here.
+
+admin.site.register(Perfil)
+admin.site.register(usuario)
--- duoc/Wiki_the_forest/models.py
+++ duoc/Wiki_the_forest/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Perfil(models.Model):
     nombre_perfil = models.CharField(max_length=200)
 
-def __str__(self):
-    return self.nombre_usuario
+    def __str__(self):
+        return self.nombre_perfil
     
 class usuario (models.Model):
     nombre_usuario = models.CharField(max_length=200, unique=True)
@@ -15,6 +15,9 @@ class usuario (models.Model):
     foto = models.ImageField(upload_to= 'usuarios/', null = True, blank=True)
     perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='usuarios')
 
-def __str__(self):
-    return self.nombre_usuario
+    def __str__(self):
+        return self.get_name_profile()
+
+    def get_name_profile(self):
+        return f" {self.nombre_usuario} - Perfil: {self.perfil.nombre_perfil}"
 
